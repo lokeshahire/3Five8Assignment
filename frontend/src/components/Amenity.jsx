0b920e2e2e2afafb37df2935275eed1e1da4d697
Lokesh <iamlokeshahire@gmail.com>
2023-08-02 13:39:41 +0530

    club

--- frontend/src/components/Amenity.jsx
+++ frontend/src/components/Amenity.jsx
@@ -3,6 +3,80 @@ import "./Amenity.css"; // Import the CSS file
 
 const Amenity = () => {
   const [bookings, setBookings] = useState([]);
+  const [config, setConfig] = useState([
+    {
+      facility: "Clubhouse",
+      slot1: { startTime: "10:00", endTime: "16:00", rate: 100 },
+      slot2: { startTime: "16:00", endTime: "22:00", rate: 500 },
+    },
+    {
+      facility: "Tennis Court",
+      rate: 50,
+    },
+  ]);
+
+  const bookFacility = (facility, date, startTime, endTime) => {
+    // check if facility is already booked
+    const existingBooking = bookings.find(
+      (booking) =>
+        booking.facility === facility &&
+        booking.date === date &&
+        ((booking.startTime >= startTime && booking.startTime < endTime) ||
+          (booking.endTime > startTime && booking.endTime <= endTime))
+    );
+
+    if (existingBooking) {
+      return { status: "Booking Failed", message: "Already Booked" };
+    }
+
+    let bookingAmount = 0; // calculate booking amount
+    const facilityConfig = config.find((conf) => conf.facility === facility);
+    if (facilityConfig.slot1 && facilityConfig.slot2) {
+      // slot based rate
+      if (
+        startTime >= facilityConfig.slot1.startTime &&
+        endTime <= facilityConfig.slot1.endTime
+      ) {
+        // falls within slot1
+        bookingAmount =
+          ((new Date(`1970-01-01T${endTime}Z`) -
+            new Date(`1970-01-01T${startTime}Z`)) /
+            36e5) *
+          facilityConfig.slot1.rate;
+      } else if (
+        startTime >= facilityConfig.slot2.startTime &&
+        endTime <= facilityConfig.slot2.endTime
+      ) {
+        // falls within slot2
+        bookingAmount =
+          ((new Date(`1970-01-01T${endTime}Z`) -
+            new Date(`1970-01-01T${startTime}Z`)) /
+            36e5) *
+          facilityConfig.slot2.rate;
+      } else {
+        const slot1Hours =
+          (new Date(`1970-01-01T${facilityConfig.slot1.endTime}Z`) -
+            new Date(`1970-01-01T${startTime}Z`)) /
+          36e5; // falls within both slots
+        const slot2Hours =
+          (new Date(`1970-01-01T${endTime}Z`) -
+            new Date(`1970-01-01T${facilityConfig.slot2.startTime}Z`)) /
+          36e5;
+        bookingAmount =
+          slot1Hours * facilityConfig.slot1.rate +
+          slot2Hours * facilityConfig.slot2.rate;
+      }
+    } else {
+      bookingAmount =
+        ((new Date(`1970-01-01T${endTime}Z`) -
+          new Date(`1970-01-01T${startTime}Z`)) /
+          36e5) *
+        facilityConfig.rate; // hour based rate
+    }
+
+    setBookings([...bookings, { facility, date, startTime, endTime }]); // add booking
+    return { status: "Booked", amount: bookingAmount };
+  };
 
   return (
     <div className="container">
